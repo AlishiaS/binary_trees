cbded6e53e5e5f5f5f7b493988cc6eb25ca3c2b2
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-21 21:33:37 +0200

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,7 +4,7 @@
  * binary_tree_balance - Measures the balance factor of a binary tree.
  * @tree: Pointer to the root node of the tree to measure the balance factor.
  *
- * Return: If tree is NULL, return 0, 
+ * Return: If tree is NULL, return 0,
  *	Else return balance factor.
  */
 int binary_tree_balance(const binary_tree_t *tree)
