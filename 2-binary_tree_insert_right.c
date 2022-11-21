99e31dfe5fa7983997965d7f2b4b44a48d5e9dae
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-21 20:24:05 +0200

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -21,8 +21,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
         if (parent->right != NULL)
         {
-                new->right = parent->left;
-                parent->right = new;
+                new->right = parent->right;
+                parent->right->parent = new;
         }
         parent->right = new;
 
