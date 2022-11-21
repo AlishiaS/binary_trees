2e023411aab4642740ebd3204653b94b85bc2353
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-21 21:31:47 +0200

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,14 +5,14 @@
  * @tree: Pointer to the root node of the tree to measure the balance factor.
  *
  * Return: If tree is NULL, return 0, 
- *      Else return balance factor.
+ *	Else return balance factor.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        if (tree)
-            return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-        return (0);
+	return (0);
 }
 
 /**
@@ -20,7 +20,7 @@ int binary_tree_balance(const binary_tree_t *tree)
  * @tree: A pointer to the root node of the tree to measure the height.
  *
  * Return: If tree is NULL, your function must return 0,
- *      Else return height.
+ *	Else return height.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
