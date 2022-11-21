e27d6436f56fe984d526fc6dedeb9ec205ad8a5f
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-11-21 21:14:15 +0200

    Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -5,11 +5,11 @@
  * @tree: pointer to the root node of the tree to count the number of leaves
  *
  * Return: 0 - If tree is NULL.
- *      NULL - if pointer is not a leaf
+ *	NULL - if pointer is not a leaf.
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-        size_t leaves = 0;
+	size_t leaves = 0;
 
 	if (tree)
 	{
